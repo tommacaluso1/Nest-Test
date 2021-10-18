3aa80487820fe1931e171ec259a0923750af0b1f
tommacaluso1 <tommcalusolavoro@hotmail.com>
2021-10-18 17:39:18 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService, UsersService],
+})
+export class AppModule {}
